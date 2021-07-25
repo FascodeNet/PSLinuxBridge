2567b3cbd048a2f4e79d25435ff94d1a45e308ff
kokkiemouse <kokkiemouse@fascode.net>
2021-07-25 20:15:30 +0900

    added rm

--- core.ps1
+++ core.ps1
@@ -17,18 +17,51 @@ function ls(){
             }
         }
     }
-
-if( $l_o -eq 1){
-    if($a_o -eq 1){
-        Get-ChildItem -Force $dirlist
+    if( $l_o -eq 1){
+        if($a_o -eq 1){
+            Get-ChildItem -Force $dirlist
+        }else{
+            Get-ChildItem $dirlist
+        }
     }else{
-        Get-ChildItem $dirlist
-    }
-}else{
-    if($a_o -eq 1){
-        Get-ChildItem -Force -Name $dirlist
-    }else{
-        Get-ChildItem -Name $dirlist
+        if($a_o -eq 1){
+            Get-ChildItem -Force -Name $dirlist
+        }else{
+            Get-ChildItem -Name $dirlist
+        }
     }
 }
+remove-item alias:rm
+function rm(){
+    $r_o=0
+    $f_o=0
+    $dirlist=@()
+    if ( $args.length -ne 0 ) {
+        foreach ( $opt in $args){
+            if( $opt[0] -eq "-" ){
+                for( $j=1; $j -lt $opt.length; $j++){
+                    switch ( $opt[$j] ){
+                        r{$r_o=1}
+                        f{$f_o=1}
+                    }
+                }
+            } else {
+                $dirlist += $opt
+            }
+        }
+    }
+    if( $r_o -eq 1){
+        if($f_o -eq 1){
+            Remove-Item -Recurse -Force $dirlist
+        }else{
+            Remove-Item -Recurse $dirlist
+        }
+    }else{
+        if($f_o -eq 1){
+            Remove-Item -Force $dirlist
+        }else{
+            
+            Remove-Item $dirlist
+        }
+    }
 }
