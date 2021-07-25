b53ba3085a0d609f532237283278cc9f874a416c
kokkiemouse <kokkiemouse@fascode.net>
2021-07-25 20:09:04 +0900

    added core.ps1

--- core.ps1
+++ core.ps1
@@ -1,4 +1,34 @@
 remove-item alias:ls
 function ls(){
-    Write-Host ls
+    $l_o=0
+    $a_o=0
+    $dirlist=@()
+    if ( $args.length -ne 0 ) {
+        foreach ( $opt in $args){
+            if( $opt[0] -eq "-" ){
+                for( $j=1; $j -lt $opt.length; $j++){
+                    switch ( $opt[$j] ){
+                        l{$l_o=1}
+                        a{$a_o=1}
+                    }
+                }
+            } else {
+                $dirlist += $opt
+            }
+        }
+    }
+
+if( $l_o -eq 1){
+    if($a_o -eq 1){
+        Get-ChildItem -Force $dirlist
+    }else{
+        Get-ChildItem $dirlist
+    }
+}else{
+    if($a_o -eq 1){
+        Get-ChildItem -Force -Name $dirlist
+    }else{
+        Get-ChildItem -Name $dirlist
+    }
+}
 }
